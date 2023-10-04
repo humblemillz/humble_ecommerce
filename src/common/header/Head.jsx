958029278ac8f53925f42d86936627ffbe0012ef
humblemillz <102180942+humblemillz@users.noreply.github.com>
2023-10-04 10:40:43 +0100

    Update Head.jsx
    update header

--- src/common/header/Head.jsx
+++ src/common/header/Head.jsx
@@ -7,9 +7,9 @@ const Head = () => {
         <div className='container d_flex'>
           <div className='left row'>
             <i className='fa fa-phone'></i>
-            <label> +88012 3456 7894</label>
+            <label> +2347063571864</label>
             <i className='fa fa-envelope'></i>
-            <label> support@ui-lib.com</label>
+            <label> contactmillionz@gmail.com</label>
           </div>
           <div className='right row RText'>
             <label>Theme FAQ"s</label>
